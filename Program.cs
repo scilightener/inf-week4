a75bbbeea82dd51073167ebae43e51e1c25fc144
scilightener <nikita.l.boss@gmail.com>
2022-10-01 02:26:36 +0300

    shitcode

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HttpServer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var myServer = new HttpServer();
+        Console.WriteLine("Exiting gracefully...");
+    }
+}
